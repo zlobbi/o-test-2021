e60017a0fa0e5279325634cdf64ae27839f778c4
kad <kmavlianov@it.gov.kg>
2021-04-24 21:14:02 +0600

    initial commit

---
+++ src/main/java/kg/km/otest2021/OTest2021Application.java
@@ -0,0 +1,13 @@
+package kg.km.otest2021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OTest2021Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OTest2021Application.class, args);
+    }
+
+}

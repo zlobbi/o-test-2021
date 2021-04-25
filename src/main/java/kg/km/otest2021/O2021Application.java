557605c99d0e8dab40c34a95be70b8d8626d7222
kad <kmavlianov@it.gov.kg>
2021-04-25 13:16:41 +0600

    controller, config

--- src/main/java/kg/km/otest2021/O2021Application.java
+++ src/main/java/kg/km/otest2021/O2021Application.java
@@ -1,13 +1,18 @@
+/*
+ * Created by Kadyrbek Mavlyanov
+ * kadyrbek.mavlyanov@gmail.com
+ * 2021.
+ */
 package kg.km.otest2021;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OTest2021Application {
+public class O2021Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(OTest2021Application.class, args);
+        SpringApplication.run(O2021Application.class, args);
     }
 
 }
---
+++ src/main/java/kg/km/otest2021/configuration/WebConfiguration.java
@@ -0,0 +1,35 @@
+/*
+ * Created by Kadyrbek Mavlyanov
+ * kadyrbek.mavlyanov@gmail.com
+ * 2021.
+ */
+package kg.km.otest2021.configuration;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/css/**")
+                .addResourceLocations("classpath:/css/");
+        registry
+                .addResourceHandler("/js/**")
+                .addResourceLocations("classpath:/js/");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH", "OPTIONS");
+    }
+
+}

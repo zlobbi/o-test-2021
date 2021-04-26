6ef9cc1ef5f963483318de060c3947366acd810e
kad <kmavlianov@it.gov.kg>
2021-04-26 19:27:35 +0600

    script updates

--- src/main/resources/js/script.js
+++ src/main/resources/js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    // Calendar defaults 
+    // Calendar defaults
     $('#calendar').fullCalendar({
         header: {
             left: 'prev,next today',
@@ -15,15 +15,21 @@ $(document).ready(function () {
         defaultDate: new Date(),
         navLinks: false, // can click day/week names to navigate views
         editable: false,
-        eventLimit: true, // allow "more" link when too many events
-        events: [
-            {
-                id: 1,
-                title: 'All Day Event',
-                start: '2021-04-24',
-                editable: true
-            }
-        ],
+        eventLimit: true, // allow "more" link when too many event
+
+        // locale
+        isRTL: false,
+        firstDay: 0,
+        monthNames: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+        monthNamesShort: ['Янв', 'Фев', 'Мар', 'Апр', 'Май', 'Июн', 'Июл', 'Авг', 'Сен', 'Окт', 'Ноя', 'Дек'],
+        dayNames: ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
+        dayNamesShort: ['Вос', 'Пон', 'Вт', 'Ср', 'Чт', 'Пят', 'Суб'],
+        buttonText: {
+            today: 'Сегодня',
+            month: 'Месяц',
+            week: 'Неделя',
+            day: 'День'
+        }
     });
 
     const calendar = document.querySelector('#calendar'),
@@ -33,10 +39,10 @@ $(document).ready(function () {
         newTaskAddBtn = document.querySelector('#new-task-btn'),
         newTask = document.querySelector('#new-task'),
         newTaskForm = document.querySelector('#new-task-form'),
-        newTaskFormSubmitBtn = document.querySelector('#new-task-form-submint-btn'),
-        newTaskFromRejectBtn = document.querySelector('#new-task-form-reject-btn');
+        newTaskFormSubmitBtn = document.querySelector('#new-task-form-submit-btn'),
+        newTaskFormRejectBtn = document.querySelector('#new-task-form-reject-btn');
 
-    newTaskFromRejectBtn.addEventListener('click', () => {
+    newTaskFormRejectBtn.addEventListener('click', () => {
         newTaskForm.reset();
         newTaskAddBtn.classList.remove('hidden');
         newTask.classList.add('hidden');
@@ -59,6 +65,19 @@ $(document).ready(function () {
         searchResult.style.display = 'none';
     });
 
+    const eventSource = fetch("/api/task/events")
+        .then((response) => response.json())
+        .then((events) => {
+            return events;
+        });
+
+    const fetchAndRenderEvents = async () => {
+        const events = await eventSource;
+        $('#calendar').fullCalendar('addEventSource', events);
+    };
+
+    fetchAndRenderEvents();
+
     function fastView(start, end) {
         var mTitle = document.querySelector('#day-title-text'),
             diff = diffBetweenStringDates(start.format('YYYY-MM-DD'), end.format('YYYY-MM-DD')),
@@ -75,5 +94,4 @@ $(document).ready(function () {
         second = new Date(second);
         return Math.abs(Math.round((second - first) / (1000 * 60 * 60 * 24)));
     }
-
 });

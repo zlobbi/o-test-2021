525f6003fb1f3a3aba2017f4465fcd65e5d7ccfc
kad <kmavlianov@it.gov.kg>
2021-04-28 17:59:47 +0600

    controllers

---
+++ src/main/java/kg/km/otest2021/controller/api/EventControllerApi.java
@@ -0,0 +1,27 @@
+/*
+ * Created by Kadyrbek Mavlyanov
+ * kadyrbek.mavlyanov@gmail.com
+ * 2021.
+ */
+package kg.km.otest2021.controller.api;
+
+import kg.km.otest2021.service.EventService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/event")
+public class EventControllerApi {
+
+    private final EventService service;
+
+    public EventControllerApi(EventService service) {
+        this.service = service;
+    }
+
+   @GetMapping("/all")
+    public String getEvents() {
+        return service.getEventsJsonString();
+   }
+}
